6aee62392280a70ec494b94f8c00be3a7f7135c4
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2026-01-20 11:15:06 +0100

    Addjusting the URL calls

--- src/Hooks/useSelectedCourses.js
+++ src/Hooks/useSelectedCourses.js
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useSchoolContext } from "../Context/SchoolContext";
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "${API_BASE_URL}";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/";
 
 // Fetch all courses and the subjects the student enrolled in
 const fectchAllCourses = async (student) => {
